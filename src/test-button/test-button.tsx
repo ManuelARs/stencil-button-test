8e48200f9d5c2b03b0791ca6adf86bedf47a4e15
Ramirez Salinas <WKYV6AN@vw.vwgroup.com>
2023-10-30 14:35:17 -0600

    feat: First button test

---
+++ src/test-button/test-button.tsx
@@ -0,0 +1,17 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+    tag: 'test-button',
+    styleUrl: 'test-button.scss',
+    shadow: true,
+})
+export class TestButton {
+    @Prop() name: string;
+
+    // function IButton({name = "", isDisabled = false, isHover = false})
+
+    render() {
+        // return <button className={isHover ? 'hover' : ''} disabled={isDisabled}>{name}</button>;
+        return <button class="buttons">{this.name}</button>;
+    }
+}   
